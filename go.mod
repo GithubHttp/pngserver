65a4ecbd84ed1973692fb59e6354dc7655c315f7
Ville Vihavainen <ville@vihavainen.eu>
2023-08-25 15:55:15 +0300

    trying some things

--- go.mod
+++ go.mod
@@ -5,9 +5,21 @@ go 1.21.0
 require github.com/kbinani/screenshot v0.0.0-20230812210009-b87d31814237
 
 require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/gen2brain/shm v0.0.0-20230802011745-f2460f5984f7 // indirect
+	github.com/gofiber/fiber/v2 v2.48.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.48.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 )
--- pngserver.go
+++ pngserver.go
@@ -2,29 +2,61 @@
 
  import (
  	"github.com/kbinani/screenshot"
- 	"image/png"
- 	"io"
-    "net/http"
-    "github.com/nfnt/resize"
- )
+ 	//"image/png"
+ 	//"io"
+    //"net/http"
+    //"github.com/nfnt/resize"
+    "github.com/gofiber/fiber/v2"
+    //"fmt"
+    "time"
+    "image"
+    "golang.org/x/image/draw"
+    "encoding/json"
+)
 
- func saveScreenshot(w io.Writer, width uint, height uint) {
-     bounds := screenshot.GetDisplayBounds(0)
 
-     img, err := screenshot.CaptureRect(bounds)
-     if err != nil {
-         panic(err)
-     }
-     resizedImg := resize.Resize(width, height, img, resize.Bilinear)
-     png.Encode(w, resizedImg)
-}
 
-func screenshotHandler(w http.ResponseWriter, r *http.Request) {
-    saveScreenshot(w, 1280, 720)
-}
+
 
 func main() {
-    http.HandleFunc("/screenshot", screenshotHandler)
+    //images := make(chan image.RGBA, 8)
 
-    http.ListenAndServe(":80", nil)
+    app := fiber.New()
+
+    app.Get("/batch/screenshot", func(c *fiber.Ctx) error {
+        img, err := screenshot.CaptureDisplay(0)
+        imgJson, _ := json.Marshal(image_2_array_pix(img))
+        // c.Set("Content-Type", "image/png")
+        c.SendString(string(imgJson))
+
+        return err
+    })
+
+    go func() {
+        for true {
+            time.Sleep(time.Millisecond * 100)
+        }
+    }()
+
+    app.Listen(":3000")
+}
+
+func image_2_array_pix(src image.Image) [][][3]float32 {
+    bounds := src.Bounds()
+    width, height := bounds.Max.X, bounds.Max.Y
+    iaa := make([][][3]float32, height)
+    src_rgba := image.NewRGBA(src.Bounds())
+    draw.Copy(src_rgba, image.Point{}, src, src.Bounds(), draw.Src, nil)
+
+    for y := 0; y < height; y++ {
+        row := make([][3]float32, width)
+        for x := 0; x < width; x++ {
+            idx_s := (y * width + x) * 4
+            pix := src_rgba.Pix[idx_s : idx_s + 4]
+            row[x] = [3]float32{float32(pix[0]), float32(pix[1]), float32(pix[2])}
+        }
+        iaa[y] = row
+    }
+
+    return iaa
 }
